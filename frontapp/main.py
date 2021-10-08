ba58a8100b525b933ffb02a50b73315f295bdbe7
Shuyi Li <shuyi.at.uu@gmail.com>
2021-10-08 22:01:01 +0200

    cloud build from cloud run

--- frontapp/main.py
+++ frontapp/main.py
@@ -507,3 +507,4 @@ app.layout = html.Div(dbc.Container(
 # Main
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0', port=8080, debug=True) #, use_reloader=False
+    # app.run_server(host='127.0.0.1', port=8080, debug=True) #, use_reloader=False
